9b442a39a8a670f45dcaf91dea3534063c64ff0a
Ch1hiro4002 <Star2947075436@gmail.com>
2026-09-09 18:49:38 +0800

    refactor: storage and get of block memory store

--- core/storage.go
+++ core/storage.go
@@ -1,17 +1,32 @@
 package core
 
+import "fmt"
+
 type Storage interface {
 	Put(*Block) error
+	Get(uint32) (*Block, error)
 }
 
 type MemoryStore struct {
-	
+	blocks map[uint32]*Block
 }
 
 func NewMemoryStore() *MemoryStore {
-	return &MemoryStore{}
+	return &MemoryStore{
+		blocks: make(map[uint32]*Block),
+	}
 }
 
 func (s *MemoryStore) Put(b *Block) error {
+	s.blocks[b.Height] = b
 	return nil
+}
+
+func (s *MemoryStore) Get(height uint32) (*Block, error) {
+	block, ok := s.blocks[height]
+	if !ok {
+		return nil, fmt.Errorf("block not found at height %d", height)
+	}
+
+	return block, nil
 }
